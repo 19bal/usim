0ec9716534e4730b502e9d4ed39ce41c04962811
seyyah <seyyah@bil.omu.edu.tr>
2011-12-15 05:31:22 +0200

    tüm iş parçalarına ait controller ve view oluşturuldu.

---
+++ a/app/rapor_v1.php
@@ -0,0 +1,13 @@
+<?php
+
+class rapor_v1 extends Home {
+	function show($params=NULL) {
+		if($this->get('dbg'))	print_pre($params, "[DEBUG] ".__CLASS__. ":".__FUNCTION__.":params");
+	}
+
+	function edit($params=NULL) {
+		if($this->get('dbg'))	print_pre($params, "[DEBUG] ".__CLASS__. ":".__FUNCTION__.":params");
+	}
+}
+
+?>
