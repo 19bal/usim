22f8d428d383d667c52e93e3e11fffdaacf7f320
seyyah <seyyah@bil.omu.edu.tr>
2011-12-15 04:23:12 +0200

    tahlil için v1-v2 eklendi

---
+++ a/app/tahlil_v1.php
@@ -0,0 +1,13 @@
+<?php
+
+class tahlil_v1 extends Home {
+	function show($params=NULL) {
+		if($this->get('dbg'))	print_pre($params, "[DEBUG] ".__CLASS__. ":".__FUNCTION__.":params");
+	}
+
+	function edit($params=NULL) {
+		if($this->get('dbg'))	print_pre($params, "[DEBUG] ".__CLASS__. ":".__FUNCTION__.":params");
+	}
+}
+
+?>
