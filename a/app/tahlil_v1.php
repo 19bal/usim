5cba910ac6b3622aec808af51c3b3ee9f791670b
seyyah <nurettin.senyer@bil.omu.edu.tr>
2011-12-29 11:45:17 +0200

    tahlil:ana-alt-daha alt doğru listeleniyor [ok]

--- a/app/tahlil_v1.php
+++ a/app/tahlil_v1.php
@@ -69,17 +69,43 @@ class tahlil_v1 extends Home {
 		$parnt = DB::sql("select * from parent$lang");
 		$survs = DB::sql("select * from survey$lang");
 
-		$a = '<ul>';
+		// < jquery-tabs function
+		$a = '<script type="text/javascript">';
+		$a .= '$(function(){';
+		$a .= '		$("#tabs").tabs();';
+		
+		foreach($discs as $dval)
+			$a .= "$('#tabs-$dval[id]').tabs();";
+
+		$a .= '});';
+		$a .= "</script>\n\n";
+		// >
+
+		$a .= "<div id='tabs'>\n";
+
+		$a .= "\t<ul>\n";
 
 		foreach($discs as $dval)
-			$a .= "<li><a href='#tabs-$dval[id]'>$dval[name]</a></li>";
+			$a .= "\t\t<li><a href='#sekme-$dval[id]'>$dval[name]</a></li>\n";
 
-		$a .= '</ul>';
+		$a .= "\t</ul>\n\n";
 
 		foreach($discs as $dval) {
 			$did = $dval['id'];
 
-			$a .= "<div id='tabs-$dval[id]'>";
+			$a .= "\t<div id='sekme-$dval[id]'>\n";
+			
+			$a .= "\t\t<div id='tabs-$did'>\n";
+			$a .= "\t\t\t<ul>\n";
+			foreach ($parnt as $pval){
+				$pid = $pval['id'];
+
+				$pdid = get_dps_id($pid, 'did');
+
+				if ($pdid == $did)
+					$a .= "\t\t\t\t<li><a href='#sekme-$pval[id]'>$pval[name]</a></li>\n";
+			}
+			$a .= "\t\t\t</ul>\n\n";
 
 			foreach ($parnt as $pval){
 				$pid = $pval['id'];
@@ -87,11 +113,14 @@ class tahlil_v1 extends Home {
 				$pdid = get_dps_id($pid, 'did');
 				$ppid = get_dps_id($pid, 'pid');
 
-				if($dbg)
-					echo "DEBUG: pid=$pid,	pdid=$pdid ve ppid=$ppid <br>";
-				/*if ($pdid == $did){
-					$a .= '<p class="answer">' . $pval['name'] . '</p>';
-					$a .= '<select multiple="multiple" size="5" name="response_'.$ppid.'[]" class="multi-select">';
+				if($dbg) echo "DEBUG: pid=$pid,	pdid=$pdid ve ppid=$ppid <br>";
+
+				if ($pdid == $did)
+				{
+					$a .= "\t\t\t<div id='sekme-$pid'>\n";
+
+					$a .= "\t\t\t\t<table border=1>\n";
+					$ss = 0;
 
 					foreach ($survs as $sval){
 						$sid = $sval['id'];
@@ -103,17 +132,29 @@ class tahlil_v1 extends Home {
 						if($dbg)
 							echo "DEBUG: sid=$sid,	sdid=$sdid, spid=$spid ve ssid=$ssid<br>";
 
+
 						if (($spid == $ppid) & ($sdid == $did)){
+							if($ss % 4 == 0)	$a .= "\t\t\t\t <tr>\n";
+							
+							$a .= "\t\t\t\t\t<td><input type='checkbox' value='$sid' name='tahlil[]' ";
 							if(array_key_exists($sid, $preselected))
-								$a .= '<option value="'. $sid .'" selected="selected">'. $sval['name'] .'</option>';
-							else
-								$a .= '<option value="'. $sid .'" >'. $sval['name'] .'</option>';
+								$a .= "checked='yes'";
+							$a .= ">$sval[name]</td>\n";
+							
+							if($ss % 4 == (4-1))	$a .= "\t\t\t\t </tr>\n";
+							$ss ++;
 						}
+
 					}
-					$a .= '</select>';
-				}*/
+					$a .= "\t\t\t\t</table>\n";
+					$a .= "\t\t\t</div>\n";
+				}
 			}
+			$a .= "\t\t</div>\n";
+			$a .= "\t</div>\n";
 		}
+		$a .= "</div>";
+
 		$this->set('tahlil_merkezi', $a);
 
 		$this->hshow('Tahlil', 'tahlil_v1_form');
